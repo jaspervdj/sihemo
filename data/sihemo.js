fdf682804457f9bce9a83042ddfb2e13134cecfd
Jasper Van der Jeugt <m@jaspervdj.be>
2012-01-30 23:46:20 +0100

    So this thing MVC is pretty cool

--- data/sihemo.js
+++ data/sihemo.js
@@ -7,93 +7,152 @@ function createWebSocket(path) {
     return new Socket(uri);
 }
 
+
+function Model() {
+    this.listeners = [];
+}
+
+Model.prototype.addChangeListener = function(listener) {
+    this.listeners.push(listener);
+};
+
+Model.prototype.triggerChange = function() {
+    for(var i in this.listeners) {
+        this.listeners[i].onChange(this);
+    }
+};
+
+
+Group.prototype = new Model();
+Group.prototype.constructor = Group;
+
 function Group(name) {
+    Model.call(this);
     this.name     = name;
     this.services = {};
+    this.allUp    = true;
+}
 
-    this.div = $(document.createElement('div'));
+Group.prototype.getService = function(json) {
+    var service;
+    if(this.services[json.id]) {
+        service = this.services[json.id];
+    } else {
+        service = new Service(json.id, json.name);
+        service.addChangeListener(this);
+        this.services[json.id] = service;
+        this.triggerChange();
+    }
+
+    return service;
+};
+
+Group.prototype.onChange = function() {
+    var allUp = true;
+
+    for(var i in this.services) {
+        allUp = allUp && this.services[i].state == 'up';
+    }
+
+    if(allUp != this.allUp) {
+        this.allUp = allUp;
+        this.triggerChange();
+    }
+};
+
+
+function GroupView(group) {
+    this.group    = group;
+    this.div      = $(document.createElement('div'));
+    this.services = {};
 
     var servicesDiv = this.servicesDiv = $(document.createElement('div'))
             .addClass('services')
             .hide();
 
     this.header = $(document.createElement('h2'))
-            .text(name)
-            .click(function () {servicesDiv.toggle(500)});
+            .text(group.name)
+            .click(function () {servicesDiv.toggle(100)});
 
     this.div.append(this.header);
     this.div.append(this.servicesDiv);
-    $('#groups').append(this.div);
 
-    this.getService = function(json) {
-        var service;
-        if(this.services[json.id]) {
-            service = this.services[json.id];
-        } else {
-            service = new Service(this, json.id, json.name);
-            this.services[json.id] = service;
+    this.group.addChangeListener(this);
+    this.onChange(group);
+}
+
+GroupView.prototype.addMissingServiceViews = function() {
+    for(var i in this.group.services) {
+        if(!this.services[i]) {
+            var serviceView = new ServiceView(this.group.services[i]);
+            this.servicesDiv.append(serviceView.div);
+            this.services[i] = serviceView;
         }
-
-        return service;
-    }
-
-    this.appendServiceDiv = function(div) {
-        this.servicesDiv.append(div);
-    }
-
-    this.update = function() {
-        var up = true;
-
-        for(var i in this.services) {
-            up = up && this.services[i].state == 'up';
-        }
-
-        this.header.removeClass('up down')
-        this.header.addClass(up ? 'up' : 'down');
     }
 }
 
-function Service(group, id, name) {
-    this.group = group;
+GroupView.prototype.onChange = function(group) {
+    this.addMissingServiceViews();
+    this.div.children('h2')
+            .removeClass('up down')
+            .addClass(group.allUp ? 'up' : 'down');
+}
+
+
+Service.prototype = new Model();
+Service.prototype.constructor = Service;
+
+function Service(id, name) {
+    Model.call(this);
     this.id    = id;
     this.name  = name;
     this.state = 'down';
+}
 
-    this.div = $(document.createElement('div'));
+Service.prototype.setState = function(state) {
+    this.state = state;
+    this.triggerChange();
+};
+
+
+function ServiceView(service) {
+    this.service = service;
+    this.div     = $(document.createElement('div'));
+
     this.div.append($(document.createElement('div'))
             .addClass('state'));
     this.div.append($(document.createElement('div'))
             .addClass('name')
-            .text(name));
+            .text(service.name));
 
-    group.appendServiceDiv(this.div);
-
-    this.update = function(state) {
-        this.state = state;
-
-        this.div.children('.state')
-                .removeClass('up down')
-                .addClass(state);
-
-        this.group.update();
-    };
+    this.service.addChangeListener(this);
 }
+
+ServiceView.prototype.onChange = function(service) {
+    this.div.children('.state')
+            .removeClass('up down')
+            .addClass(service.state);
+}
+
 
 function GroupManager() {
     this.groups = {};
-
-    this.getService = function(json) {
-        var group;
-        if(this.groups[json.group]) {
-            group = this.groups[json.group];
-        } else {
-            group = new Group(json.group);
-            this.groups[json.group] = group;
-        }
-
-        return group.getService(json);
-    }
 }
+
+GroupManager.prototype.getService = function(json) {
+    var group;
+    if(this.groups[json.group]) {
+        group = this.groups[json.group];
+    } else {
+        group = new Group(json.group);
+        groupView = new GroupView(group);
+        $('#groups').append(groupView.div);
+        this.groups[json.group] = group;
+    }
+
+    return group.getService(json);
+}
+
 
 $(document).ready(function() {
     var groupManager = new GroupManager();
@@ -101,7 +160,7 @@ $(document).ready(function() {
     $.get('services.json', function(json) {
         for(var i in json) {
             var service = groupManager.getService(json[i].service);
-            service.update(json[i].state);
+            service.setState(json[i].state);
         }
 
         var ws = createWebSocket('/subscribe');
@@ -109,7 +168,7 @@ $(document).ready(function() {
         ws.onmessage = function(event) {
             var json = JSON.parse(event.data);
             var service = groupManager.getService(json.service);
-            service.update(json.state);
+            service.setState(json.state);
         };
 
         ws.onerror = function(event) {
--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,13 @@ module Main
     ( main
     ) where
 
+import Control.Monad (forM_)
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Monad (forever)
 import System.Random (randomRIO)
 
 import qualified Data.Aeson as A
+import qualified Data.Text as T
 import qualified Network.WebSockets as WS
 import qualified Network.WebSockets.Util.PubSub as WS
 
@@ -15,17 +17,20 @@ import Sihemo.Types
 import Sihemo.Monitor
 import Sihemo.Web
 
-demo :: Monitor -> IO ()
-demo monitor = forever $ do
+demo :: Monitor -> Int -> IO ()
+demo monitor nr = forever $ do
     heartbeat monitor $ Heartbeat service 5
-    random <- randomRIO (2, 10)
+    random <- randomRIO (2, 6)
     threadDelay $ random * 1000 * 1000
   where
-    service = Service "demo" "demo-1" "Demo 1"
+    service = Service "demo" ("demo-" `T.append` nr') ("Demo " `T.append` nr')
+    nr'     = T.pack (show nr)
 
 main :: IO ()
 main = do
     pubSub  <- WS.newPubSub
     monitor <- newMonitor $ WS.publish pubSub . WS.textData . A.encode
-    _       <- forkIO $ demo monitor
+    forM_ [1 .. 10] $ \i -> do
+        _ <- forkIO $ demo monitor i
+        return ()
     serve monitor pubSub
