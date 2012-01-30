813184bdda72484db8bdfe0be2aecf832ab00699
Jasper Van der Jeugt <m@jaspervdj.be>
2012-01-30 16:27:39 +0100

    Write some javascript

--- data/sihemo.js
+++ data/sihemo.js
@@ -7,6 +7,69 @@ function createWebSocket(path) {
     return new Socket(uri);
 }
 
+function Group(name) {
+    this.name     = name;
+    this.services = {};
+
+    this.div = $(document.createElement('div'));
+    this.div.append($(document.createElement('h2')).text(name));
+    $('#groups').append(this.div);
+
+    this.getService = function(json) {
+        var service;
+        if(this.services[json.id]) {
+            service = this.services[json.id];
+        } else {
+            service = new Service(this, json.id, json.name);
+            this.services[json.id] = service;
+        }
+
+        return service;
+    }
+}
+
+function Service(group, id, name) {
+    this.group = group;
+    this.id    = id;
+    this.name  = name;
+    this.state = 'down';
+
+    this.div = $(document.createElement('div'));
+    this.div.append($(document.createElement('div')).text(name));
+    this.div.append($(document.createElement('div'))
+        .addClass('state')
+        .text(this.state));
+
+    $('#groups').append(this.div);
+
+    this.update = function(state) {
+        this.state = state;
+    };
+}
+
+function GroupManager() {
+    this.groups = {};
+
+    this.getService = function(json) {
+        var group;
+        if(this.groups[json.group]) {
+            group = this.groups[json.group];
+        } else {
+            group = new Group(json.group);
+            this.groups[json.group] = group;
+        }
+
+        return group.getService(json);
+    }
+}
+
 $(document).ready(function() {
-    $('#groups').append('hi');
+    var groupManager = new GroupManager();
+
+    $.get('services.json', function(json) {
+        for(var i in json) {
+            var service = groupManager.getService(json[i].service);
+            service.update(json[i].state);
+        }
+    });
 });
--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main
     ( main
     ) where
 
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Monad (forever)
+import System.Random (randomRIO)
+
+import Sihemo.Types
 import Sihemo.Monitor
 import Sihemo.Web
+
+demo :: Monitor -> IO ()
+demo monitor = forever $ do
+    heartbeat monitor $ Heartbeat service 5
+    random <- randomRIO (2, 10)
+    threadDelay $ random * 1000 * 1000
+  where
+    service = Service "demo" "demo-1" "Demo 1"
 
 main :: IO ()
 main = do
     monitor <- newMonitor $ \_ _ -> return ()
+    _       <- forkIO $ demo monitor
     serve monitor
