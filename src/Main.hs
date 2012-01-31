2e477fda9edb18ecd5db5db85a48a17e0e69083e
Jasper Van der Jeugt <m@jaspervdj.be>
2012-01-31 16:16:25 +0100

    Add a runSihemo function

--- src/Main.hs
+++ src/Main.hs
@@ -6,15 +6,11 @@ module Main
 import Control.Monad (forM_)
 import System.Random (randomRIO)
 
-import qualified Data.Aeson as A
 import qualified Data.Text as T
-import qualified Network.WebSockets as WS
-import qualified Network.WebSockets.Util.PubSub as WS
 
+import Sihemo
 import Sihemo.Client
-import Sihemo.Monitor
 import Sihemo.Types
-import Sihemo.Web
 
 randomCheck :: IO Bool
 randomCheck = do
@@ -22,15 +18,10 @@ randomCheck = do
     return $ random > 0
 
 main :: IO ()
-main = do
-    pubSub  <- WS.newPubSub
-    monitor <- newMonitor $ WS.publish pubSub . WS.textData . A.encode
-
+main = runSihemoWith (\_ -> return ()) $ \monitor ->
     forM_ [1 :: Int .. 5] $ \i ->
         forM_ (services i) $ \s ->
             checkEvery monitor s 10 12 randomCheck
-
-    serve monitor pubSub
   where
     services nr = map (Service group) ["usb", "gyrid", "ping", "ssh"]
       where
---
+++ src/Sihemo.hs
@@ -0,0 +1,37 @@
+module Sihemo
+    ( runSihemo
+    , runSihemoWith
+    ) where
+
+import qualified Data.Aeson as A
+import qualified Network.WebSockets as WS
+import qualified Network.WebSockets.Util.PubSub as WS
+
+import Sihemo.Monitor
+import Sihemo.Types
+import Sihemo.Web
+
+-- | Start the web server. This function blocks forever.
+runSihemo :: IO ()
+runSihemo = runSihemoWith noHook noHook
+  where
+    noHook _ = return ()
+
+-- | A variant of 'runSihemo' which allows you to pass some custom hooks.
+--
+-- The snapshot hook is called every time the status of some service changes.
+--
+-- The setup hook is only called once at initialization. It allows you to
+-- register some custom checks with the monitor.
+runSihemoWith :: (ServiceSnapshot -> IO ())  -- ^ Snapshot hook
+              -> (Monitor -> IO ())          -- ^ Setup hook
+              -> IO ()                       -- ^ Blocks forever
+runSihemoWith hook setupMonitor = do
+    pubSub  <- WS.newPubSub
+    monitor <- newMonitor $ hook' pubSub
+    setupMonitor monitor
+    serve monitor pubSub
+  where
+    hook' pubSub snapshot = do
+        WS.publish pubSub $ WS.textData $ A.encode snapshot
+        hook snapshot
