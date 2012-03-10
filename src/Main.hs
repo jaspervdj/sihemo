995313f4b176d6e24a32271c0578e424d6e4ef84
Jasper Van der Jeugt <m@jaspervdj.be>
2012-03-10 11:15:05 +0100

    Remove random gyrid data

--- src/Main.hs
+++ src/Main.hs
@@ -3,26 +3,7 @@ module Main
     ( main
     ) where
 
-import Control.Monad (forM_)
-import System.Random (randomRIO)
-
-import qualified Data.Text as T
-
-import Sihemo
-import Sihemo.Client
-import Sihemo.Types
-
-randomCheck :: IO Bool
-randomCheck = do
-    random <- randomRIO (0 :: Int, 10)
-    return $ random > 0
+import Sihemo (runSihemo)
 
 main :: IO ()
-main = runSihemoWith (\_ -> return ()) $ \monitor ->
-    forM_ [1 :: Int .. 5] $ \i ->
-        forM_ (services i) $ \s ->
-            checkEvery monitor s 10 12 randomCheck
-  where
-    services nr = map (Service group) ["usb", "gyrid", "ping", "ssh"]
-      where
-        group = "gyrid-" `T.append` (T.pack (show nr))
+main = runSihemo
